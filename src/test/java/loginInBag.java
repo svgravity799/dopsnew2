ab987278f55a86e25d2eeda911edeb9f2f3241bd
pavelyatmanov <svgravity799@gmail.com>
2025-06-12 18:21:16 -0700

    n1ceUpdate

--- src/test/java/loginInBag.java
+++ src/test/java/loginInBag.java
@@ -29,40 +29,42 @@ Configuration.holdBrowserOpen = true;
 
 Selenide.open("https://fstravel.com/searchexcursions");
 
+
+//Выполнение поиска
 $("div[class=v-departure__pinput]").click();
 $$("[data-v-7cc930c8].v-departure__elem").findBy(Condition.text("Турция")).click();
-
-sleep(1000);
+             sleep(1000);
 $("[data-v-08a2bd6c].calendar__field-title").click();
-$("[class=calendar-popup__arrow-right]").click();
-$("[class=calendar-popup__arrow-right]").click();
-$("[class=calendar-popup__arrow-right]").click();
-$("[class=calendar-popup__arrow-right]").click();
-
+  $("[class=calendar-popup__arrow-right]").click();
+  $("[class=calendar-popup__arrow-right]").click();
+  $("[class=calendar-popup__arrow-right]").click();
+  $("[class=calendar-popup__arrow-right]").click();
 $(byText("2")).click();
 $(byText("10")).click();
-
 $("[data-v-4fdad086].v-tourists__pinput").click();
 $("[data-v-4fdad086].v-icon-minus_square").click();
 $("button.v-search-button").click();
+System.out.println("Поиск выполнен");
 
         //Закрытие всплывающего Попап
         if ($$("div.popmechanic-close").size() > 0) {
             $("div.popmechanic-close").click();
         }
-
 
 sleep(2000);
 
         //Закрытие всплывающего Попап
-        if ($$("div.popmechanic-close").size() > 0) {
+        if ($$("div.popmechanic-close").size() > 0)
+        {
             $("div.popmechanic-close").click();
         }
 
-
-        ElementsCollection cards_of_vidacha = $$("div.excursion-card__content").shouldHave(CollectionCondition.sizeGreaterThan(1));  // Ждём, пока появятся карточки
+//Выбор карточки из выдачи
+ElementsCollection cards_of_vidacha = $$("div.excursion-card__content").shouldHave(CollectionCondition.sizeGreaterThan(1));  // Ждём, пока появятся карточки
 cards_of_vidacha.get(1).$("button").click();
+System.out.println("Выдача отображается");
 
+//Открытие календаря и выбор 9го числа
 $("div[data-v-26c18107].calendar__field-dates").click();
 $$("[data-v-26c18107].calendar-popup__text").findBy(Condition.text("9")).click();
 
@@ -72,29 +74,28 @@ $$("[data-v-26c18107].calendar-popup__text").findBy(Condition.text("9")).click()
         }
 
 
-   sleep(1000);
+sleep(1000);
 
         //Закрытие всплывающего Попап
-        if ($$("div.popmechanic-close").size() > 0) {
+        if ($$("div.popmechanic-close").size() > 0)
+        {
             $("div.popmechanic-close").click();
         }
 
+System.out.println("Попап экскурсии открыт, дата выбрана");
 
-        $("button[class=excursion-modal-main__buttons-btn]").shouldHave(Condition.visible).click();
+//Добавление в корзину
+$("button[class=excursion-modal-main__buttons-btn]").shouldHave(Condition.visible).click();
    //sleep(1000);
 sleep(10000);
 
 switchTo().window(1);
 
 
-
-//Закрытие всплывающего Попап
+        //Закрытие всплывающего Попап
         if ($$("div.popmechanic-close").size() > 0) {
             $("div.popmechanic-close").click();
         }
-
-
-
 
 
 sleep(3000);
@@ -105,68 +106,35 @@ sleep(3000);
         }
 
 
+System.out.println("Переход в новую вкладку выполнен, экскурсия добавлена в корзину");
 
+//Выполнение авторизации
 $("div[data-v-17ed79cc].v-btn-yellow").click();
 $("[type=email]").setValue("testfsJavaTest@yandex.ru"); // Вводим логин
 $("[type=password]").setValue("Abonent13!");
-$("[type=submit]").click();  // Вводим пароль
+$("[type=submit]").click();
 
-        sleep(1000);
+sleep(1000);
 
-        $("input[id=place][name=place]").shouldBe(Condition.visible).setValue("Автотесты улица");
+System.out.println("Авторизация в корзине выполнена");
 
-        sleep(1000);
+//Заполнение данных о туристах
+$("input[id=place][name=place]").shouldBe(Condition.visible).setValue("Автотесты улица");
+sleep(1000);
+$("#lastName").shouldBe(Condition.visible, Duration.ofSeconds(5)).setValue("АвтоИванов");
+   sleep(1000);
+$("#firstName").shouldBe(Condition.visible, Duration.ofSeconds(5)).setValue("АвтоСергей");
+   sleep(1000);
+$("#middleName").shouldBe(Condition.visible, Duration.ofSeconds(5)).setValue("АвтоИванович");
+   sleep(1000);
+$("#lastName").shouldBe(Condition.visible, Duration.ofSeconds(5)).setValue("АвтоИванов");
+$(byText("Номер документа")).parent().$("input").setValue("124416");
+$(byText("Cерия документа")).parent().$("input").setValue("1234");
 
-
-        $("#lastName").shouldBe(Condition.visible, Duration.ofSeconds(5)).setValue("АвтоИванов");
-        sleep(1000);
-        $("#firstName").shouldBe(Condition.visible, Duration.ofSeconds(5)).setValue("АвтоСергей");
-        sleep(1000);
-        $("#middleName").shouldBe(Condition.visible, Duration.ofSeconds(5)).setValue("АвтоИванович");
-        sleep(1000);
-        $("#lastName").shouldBe(Condition.visible, Duration.ofSeconds(5)).setValue("АвтоИванов");
-
-
-        $(byText("Номер документа")).parent().$("input").setValue("1244");
-        $(byText("Cерия документа")).parent().$("input").setValue("123422");
-
-
+System.out.println("Данные о туристах в корзине заполнены");
 
 
 
-
-        // $(byText("Cерия документа")).setValue("5723");
-
-
-      //  $("input[name='id']").setValue("1234");
-
-
-        //  $("[data-v-1f43af2e].form__label").shouldBe(Condition.visible, Duration.ofSeconds(5)).setValue("25").click();
-
-       // $$("[data-v-1f43af2e].form__field_active").setValue("5235");
-
-
-
-      //  $(byText("Серия документа")).shouldBe(Condition.visible, Duration.ofSeconds(5)).setValue("5412");
-
-
-        //  $$("input[name='id']").get(1).setValue("567890"); // выбирает второй input с name="id"
-
-
-        // $("input[placeholder='Серия документа']").setValue("1234");
-     //   $("input[place=holder='Номер документа']").setValue("881177");
-
-
-
-
-
-        // $("[data-v-1f43af2e.form__label]").setValue("Тест");
-
-
-      //  $("input[id=lastName][name=lastName]").shouldBe(Condition.visible).setValue("ТестАвтотесты");
-     //   $("input[id=lastName][name=firstName]").shouldBe(Condition.visible).setValue("Петров");
-    //    $("input[id=lastName][name=middleName]").shouldBe(Condition.visible).setValue("Артем");
-      $(byText("Серия документа")).shouldBe(Condition.visible).setValue("22");
 
 
     }

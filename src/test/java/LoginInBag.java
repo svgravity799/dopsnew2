a7b831df717f155a545122a2403f887a89cb5e08
pavelyatmanov <svgravity799@gmail.com>
2025-08-10 10:38:40 +0400

    lalati

--- src/test/java/LoginInBag.java
+++ src/test/java/LoginInBag.java
@@ -21,6 +21,10 @@ import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 public class
 LoginInBag {
 
+    @Before
+    public void setup() {
+        WebDriverManager.chromedriver().setup();  // <--- вот здесь
+    }
 
     @Test
     @Story("Бронирование экскурсий с авторизацией в корзине")
